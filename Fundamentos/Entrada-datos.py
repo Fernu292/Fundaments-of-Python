8b7c97dac2fbf510efe03debc067c84e704a5101
Fernu292 <fernu292@gmail.com>
2021-08-25 15:26:30 -0500

    Fundaments of Python
    Fundaments of data entry

--- Fundamentos/Entrada-datos.py
+++ Fundamentos/Entrada-datos.py
@@ -1,2 +1,23 @@
-resultado = input("Ingresa un valor")
-print("El resultado es: ",resultado)
+
+#Para poder tomar datos como un input de un usuario podremos hacerlo 
+#al igualar la variable donde guardaremos el dato 
+#a un input() en este podremos mostrar un mensaje que le diga al usuario
+#que hacer
+
+
+dato = input("Ingresa un dato: ")
+print(f'El dato es: {dato}')
+
+
+#Input() con tipo del dato, si queremos tomar especificamente un tipo de
+#dato lo haremos con la funcion del mismo nombre del tipo de dato 
+
+
+#Tipo entero
+numero = int(input('Digita un numero: '))
+print(f'El numero es: {numero}')
+
+#Tipo string 
+
+mensaje = str(input("Digita un mensaje: "))
+print(f'El mensaje es: {mensaje}')
